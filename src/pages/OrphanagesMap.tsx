78d4beea187ae78ed276f1428011555c201224c0
Edipo Elwes <edipoelwes2@ufpi.edu.br>
2020-10-14 12:45:25 -0300

    Criando rotas e navegaçao entre paginas

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const OrphanagesMap = () => {
+  return <div>orfanatos</div>
+}
+
+export default OrphanagesMap
---
+++ src/routes.tsx
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Landing from './pages/Landing'
+import OrphanagesMap from './pages/OrphanagesMap'
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/app" component={OrphanagesMap} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Routes
